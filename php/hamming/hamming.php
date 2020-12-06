dc8bb1019b2e2b20fded1a7562c23ff63672383d
ozor <ozor@ngs.ru>
2020-12-06 14:56:06 +0700

    Implement "Hamming" task for the PHP track

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,15 +1,17 @@
 <?php
 
-/*
-This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-convenience to get you started writing code faster.
-
-Remove this comment before submitting your exercise.
-*/
-
 function distance(string $strandA, string $strandB) : int
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (strlen($strandA) != strlen($strandB)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+
+    $distance = 0;
+    for ($i = 0; $i < strlen($strandA); $i++) {
+        if ($strandA[$i] != $strandB[$i]) {
+            $distance++;
+        }
+    }
+
+    return $distance;
 }
