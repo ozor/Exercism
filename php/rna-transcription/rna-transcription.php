8704a143841d88804ae48bca38a21209cb351c6d
ozor <ozor@ngs.ru>
2020-12-08 21:21:09 +0700

    Implement the "RNA Transcription" task for the PHP track

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -1,1 +1,17 @@
 <?php
+
+function toRna(string $dna): string
+{
+    $replacements = [
+        'G' => 'C',
+        'C' => 'G',
+        'T' => 'A',
+        'A' => 'U',
+    ];
+
+    $nucleotides = array_map(function($nucleotide) use ($replacements) {
+        return $replacements[$nucleotide];
+    }, str_split($dna));
+
+    return implode('', $nucleotides);
+}
