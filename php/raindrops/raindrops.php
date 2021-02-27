98c0e396e0457f144c3ae9e3d6a164605f39ef95
ozor <ozor@yandex.ru>
2021-02-27 18:28:32 +0700

    Implement "Raindrops" task for the PHP track

--- php/raindrops/raindrops.php
+++ php/raindrops/raindrops.php
@@ -1,1 +1,18 @@
 <?php
+
+function raindrops(int $number): string {
+    $numbers = [
+        3 => 'Pling',
+        5 => 'Plang',
+        7 => 'Plong',
+    ];
+
+    $raindrop = '';
+    foreach ($numbers as $key => $item) {
+        if ($number % $key === 0) {
+            $raindrop .= $numbers[$key];
+        }
+    }
+
+    return $raindrop ?: $number;
+}
