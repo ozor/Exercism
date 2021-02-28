d5be6e02bd59fb4e2f63cc5be7877d6e3f1a66f1
ozor <ozor@yandex.ru>
2021-02-28 11:33:58 +0700

    Implement "Word Count" task for the PHP track

--- php/word-count/word-count.php
+++ php/word-count/word-count.php
@@ -1,1 +1,17 @@
 <?php
+
+function wordCount(string $phrase): array {
+    $wordCounter = [];
+
+    $words = str_word_count(strtolower($phrase), 1, 1234567890);
+
+    foreach ($words as $word) {
+        if (isset($wordCounter[$word])) {
+            $wordCounter[$word]++;
+        } else {
+            $wordCounter[$word] = 1;
+        }
+    }
+
+    return $wordCounter;
+}
