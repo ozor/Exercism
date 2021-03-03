05aeb2514392987aa02c488c62ac8b0c9abd2a77
ozor <ozor@yandex.ru>
2021-03-03 23:49:01 +0700

    Implement "Raindrops" task for the Python track

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,16 @@
 def convert(number):
-    pass
+    numbers = {
+        3: 'Pling',
+        5: 'Plang',
+        7: 'Plong'
+    }
+
+    raindrop = ''
+    for key in numbers:
+        if number % key == 0:
+            raindrop += numbers[key]
+
+    if raindrop == '':
+        raindrop = number
+
+    return str(raindrop)
