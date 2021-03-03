22adb547cf74ae383eaaf1ae72419c0103ec3138
ozor <ozor@yandex.ru>
2021-03-03 13:36:40 +0700

    Implement "Pangram" task for the PHP track

--- php/pangram/pangram.php
+++ php/pangram/pangram.php
@@ -1,1 +1,11 @@
 <?php
+
+function isPangram(string $sentence): bool
+{
+    $sentence = preg_replace('/[\s\d\W_.-]*/i', '', strtolower($sentence));
+
+    $patternArray  = range('a', 'z'); //mb_str_split('abcdefghigklmopqrstuvwxyz');
+    $sentenceArray = array_unique(mb_str_split($sentence));
+
+    return count(array_diff($patternArray, $sentenceArray)) === 0;
+}
