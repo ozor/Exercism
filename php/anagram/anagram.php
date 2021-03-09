47b706b742e0ee4e070e76ee42466dad7ee40648
ozor <ozor@yandex.ru>
2021-03-09 22:26:49 +0700

    Implement "Anagram" task for the PHP track

--- php/anagram/anagram.php
+++ php/anagram/anagram.php
@@ -1,1 +1,33 @@
 <?php
+
+function detectAnagrams(string $pattern, array $words): array
+{
+    $anagrams = [];
+
+    $pattern = mb_strtolower($pattern);
+    $patternArray = mb_str_split($pattern);
+    sort($patternArray, SORT_LOCALE_STRING);
+
+    foreach ($words as $word) {
+        $_word = mb_strtolower($word);
+        if ($_word == $pattern || strlen($word) != strlen($pattern)) {
+            continue;
+        }
+
+        $wordArray = mb_str_split($_word);
+        sort($wordArray, SORT_LOCALE_STRING);
+
+        $count = 0;
+        foreach ($wordArray as $key => $letter) {
+            if ($wordArray[$key] == $patternArray[$key]) {
+                $count++;
+            }
+        }
+
+        if (count($patternArray) == $count) {
+            $anagrams[] = $word;
+        }
+    }
+
+    return $anagrams;
+}
