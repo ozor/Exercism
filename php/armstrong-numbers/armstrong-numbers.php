0ab37086903ee70e3370cd758c74bb98b295a4e8
ozor <ozor@yandex.ru>
2021-03-10 12:17:03 +0700

    Implement "Armstrong Numbers" task for the PHP track

--- php/armstrong-numbers/armstrong-numbers.php
+++ php/armstrong-numbers/armstrong-numbers.php
@@ -1,1 +1,18 @@
 <?php
+
+function isArmstrongNumber(int $number): bool
+{
+    if ($number === 0) {
+        return true;
+    }
+
+    $numberArray = str_split((string)$number);
+    $countDigits = count($numberArray);
+
+    $sumOfDigits = array_reduce($numberArray, function ($carry, $item) use ($countDigits) {
+        $carry += (int)$item ** $countDigits;
+        return $carry;
+    }, 0);
+
+    return $sumOfDigits === $number;
+}
