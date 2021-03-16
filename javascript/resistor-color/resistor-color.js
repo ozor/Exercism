e9bc54509010df540e60b7f95cb4a971d695918d
ozor <ozor@yandex.ru>
2021-03-16 11:48:02 +0700

    Implement "Resistor Color" task for the JavaScript track

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,10 +1,21 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (colorName) => {
+  for (let item in COLORS) {
+    if (COLORS[item] === colorName) {
+      return parseInt(item)
+    }
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
