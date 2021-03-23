c500c92ccef8e715fe96d510d84bc14f92ac7e61
ozor <ozor@yandex.ru>
2021-03-23 13:00:22 +0700

    Implement "Difference Of Squares" task for the PHP track

--- php/difference-of-squares/difference-of-squares.php
+++ php/difference-of-squares/difference-of-squares.php
@@ -1,1 +1,20 @@
 <?php
+
+function squareOfSum(int $number): int
+{
+    return array_sum(range(1, $number)) ** 2;
+}
+
+function sumOfSquares(int $number): int
+{
+    return array_sum(
+        array_map(function ($number) {
+            return $number ** 2;
+        }, range(1, $number))
+    );
+}
+
+function difference(int $number): int
+{
+    return squareOfSum($number) - sumOfSquares($number);
+}
