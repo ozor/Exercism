c62a67e7c3fa289f5e5d47a6326d62e817b40cfc
ozor <ozor@yandex.ru>
2021-04-01 21:04:35 +0700

    Implement "High Scores" task for the Python track

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,19 @@
+
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    if len(scores) < 3:
+        return sorted(scores, reverse=True)
+
+    result = []
+    while len(result) < 3:
+        index = list.index(scores, max(scores))
+        result.append(scores.pop(index))
+
+    return sorted(result, reverse=True)
