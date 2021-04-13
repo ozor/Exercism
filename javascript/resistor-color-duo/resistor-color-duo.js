154d9186380798ac1fc35d2b4b4570ef824abd3a
ozor <ozor@yandex.ru>
2021-04-13 18:34:46 +0700

    Implement "Resistor Color Duo" task for the JavaScript track

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const decodedValue = (colorNames) => parseInt(COLORS.indexOf(colorNames[0]).toString() + COLORS.indexOf(colorNames[1]).toString());
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
