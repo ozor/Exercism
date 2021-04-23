52f9c26238c0f28623fe48a2f87e6592e66022fd
ozor <ozor@yandex.ru>
2021-04-23 13:09:32 +0700

    Improve "Matrix" task for the Python track

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,19 +1,11 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.matrix = []
-        rows = matrix_string.split("\n")
-        for row in rows:
-            columns = row.split()
-            parsed = []
-            for column in columns:
-                parsed.append(int(column))
-            self.matrix.append(parsed)
+        for row in matrix_string.splitlines():
+            self.matrix.append([int(column) for column in row.split()])
 
     def row(self, index):
         return self.matrix[index-1]
 
     def column(self, index):
-        column = []
-        for row in self.matrix:
-            column.append(row[index-1])
-        return column
+        return [row[index-1] for row in self.matrix]
