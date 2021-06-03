0cdda3f1e81d082c8f2e686cae4e3f405781796c
ozor <ozor@yandex.ru>
2021-06-03 21:16:02 +0700

    Implement "Trinary" task for the PHP track

--- php/trinary/trinary.php
+++ php/trinary/trinary.php
@@ -1,1 +1,17 @@
 <?php
+
+function toDecimal(string $number): int
+{
+    $allowedNumbers = [0, 1, 2];
+    $numberArray = array_reverse(str_split($number));
+
+    $result = 0;
+    foreach ($numberArray as $i => $item) {
+        if (!in_array($item, $allowedNumbers)) {
+            return 0;
+        }
+        $result += $item * pow(3, $i);
+    }
+
+    return $result;
+}
