7d8824fadfbe8acc093502a2238697af5f3e0f7b
ozor <ozor@yandex.ru>
2021-06-03 23:55:57 +0700

    Implement "Hamming" task for the Python track

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError('Lengths are not equal')
+
+    if len(strand_a) == 0 and len(strand_b) == 0:
+        return 0
+
+    distance = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            distance += 1
+
+    return distance
