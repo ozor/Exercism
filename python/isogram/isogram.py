e7957aea0bce11809c129b44623a619c134dfda3
ozor <ozor@yandex.ru>
2021-06-04 21:25:21 +0700

    Implement "Isogram" task for the Python track

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,9 @@
 def is_isogram(string):
-    pass
+    list_of_strings = []
+    for letter in string:
+        if letter == '-' or letter == ' ':
+            continue
+        if letter.lower() in list_of_strings:
+            return False
+        list_of_strings.append(letter.lower())
+    return True
