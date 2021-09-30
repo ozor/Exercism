32d115cd2e2ba6004c6e342c896f66b88e0ff641
ozor <ozor@yandex.ru>
2021-09-30 10:39:00 +0700

    Update "Two Fer" task for the Bash track

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
 main () {
-  if (( $# >= 1 ))
-  then
-    name=$1
-  else
-    name="you"
-  fi
-
-  printf "One for %s, one for me.\n" "$name"
+  printf "One for %s, one for me.\n" "${1:-you}"
 }
 
 main "$@"
